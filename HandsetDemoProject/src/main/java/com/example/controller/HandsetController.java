71d63c9e7fb7f0fe192c2a51a5b0462a349788cf
gupta_k <gupta_k@Kritika-GT.India.XoriantCorp.com>
2019-10-04 11:55:49 +0530

    import warning remove

--- HandsetDemoProject/src/main/java/com/example/controller/HandsetController.java
+++ HandsetDemoProject/src/main/java/com/example/controller/HandsetController.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
